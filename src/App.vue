b413e61b4245c2abb1454845c5a4b2fd7c3abf43
naveen-ranium <naveen@ranium.in>
2024-12-16 11:58:10 +0530

    Initialized Project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-semibold underline">
+    Hello world!
+  </h1>
+</template>
